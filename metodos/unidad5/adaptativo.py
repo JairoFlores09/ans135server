4435a76b7c0179ea5d31c16ad5ece4f98d879879
JairoFlores <jairosfcastro090520@gmail.com>
2022-06-17 14:35:52 -0600

    Correccion del metodo multipasos

--- metodos/unidad5/adaptativo.py
+++ metodos/unidad5/adaptativo.py
@@ -32,33 +32,28 @@ class Adaptativo(RungeKutta):
 
 		x = list(map(lambda val: float(val),x))
 		ykutta = list(map(lambda val: float(val), ykutta))
+
+		valores = []
+
+		for k in range(self.pasos):
+			valores.append(ykutta[k])
+
+		for i in range(self.pasos - 1, int(self.n)):
+			if self.pasos == 4:
+				predictor = ykutta[i] + ((self.h/24)*(55*(self.eval(x[i],ykutta[i])) - 59*(self.eval(x[i-1],ykutta[i-1])) + 37*(self.eval(x[i-2],ykutta[i-2])) - 9*(self.eval(x[i-3],ykutta[i-3]))))
+				corrector = ykutta[i] + ((self.h/24)*(9*(self.eval(x[i+1],predictor)) + 19*(self.eval(x[i],ykutta[i])) - 5*(self.eval(x[i-1],ykutta[i-1])) + (self.eval(x[i-2],ykutta[i-2]))))
+			elif self.pasos == 3:
+				predictor = ykutta[i] + ((self.h/12)*(23*(self.eval(x[i],ykutta[i])) - 16*(self.eval(x[i-1],ykutta[i-1])) + 5*(self.eval(x[i-2],ykutta[i-2]))))
+				corrector = ykutta[i] + ((self.h/12)*(5*(self.eval(x[i+1],predictor)) + 8*(self.eval(x[i],ykutta[i])) - (self.eval(x[i-1],ykutta[i-1]))))
+			else:
+				predictor = ykutta[i] + ((self.h/2)*(3*(self.eval(x[i],ykutta[i])) - (self.eval(x[i-1],ykutta[i-1]))))
+				corrector = ykutta[i] + ((self.h/2)*((self.eval(x[i+1],predictor)) + (self.eval(x[i],ykutta[i]))))
+
+			valores.append(corrector)
 		
-		values = []
-		for i in range(len(x) - 1):
-			values.append(self.eval(x[i],ykutta[i]))
-
-		if self.pasos == 4:
-			predictor = ykutta[self.pasos - 1] + ((self.h/24)*(55*values[-1] - 59*values[-2] + 37*values[-3] - 9*values[-4]))
-		elif self.pasos == 3:
-			predictor = ykutta[self.pasos - 1] + ((self.h/12)*(23*values[-1] - 16*values[-2] + 5*values[-3]))
-		else:
-			predictor = ykutta[self.pasos - 1] + ((self.h/2)*(3*values[-1] - values[-2]))
-
-		values.append(self.eval(self.xf,predictor))
-
-		if self.pasos == 4:
-			corrector = ykutta[self.pasos - 1] + ((self.h/24)*(9*values[-1] + 19*values[-2] - 5*values[-3] + values[-4]))
-		elif self.pasos == 3:
-			corrector = ykutta[self.pasos - 1] + ((self.h/12)*(5*values[-1] + 8*values[-2] - values[-3]))
-		else:
-			corrector = ykutta[self.pasos - 1] + ((self.h/2)*(values[-1] + values[-2]))
-
-		values.pop() # se elimina el elemento del predictor
-		values.append(corrector) # y se agrega el elemento del corrector
-
 		resultado = {
-			'x': list(map(lambda val: str(float(val)),x)),
-			'yadaptativo': list(map(lambda val: str(float(val)), values))
+			'x': list(map(lambda val: str(float(val)), x)),
+			'yadaptativo': list(map(lambda val: str(float(val)), valores))
 		}
 
 		return resultado
